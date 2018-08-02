8e2104a5998e40fc380aeb029be6f89cc7d62dfb
Cosmin Filip <cosmin.filip@connatix.com>
2018-08-02 14:29:54 +0300

    new commit

--- script1.py
+++ script1.py
@@ -1,9 +1,18 @@
-#!/usr/bin/env python3
+import datetime
+import boto3
 
-print ("Hello Cosmin")
+client = boto3.client('athena')
 
-f = open("cosmin.txt", "w")
-for i in range(10):
-    f.write("This is line %d\r\n" % (i+1))
-    f.close
-    
+response = client.batch_get_query_execution(
+	QueryExecutionIds=[
+		'SHOW SCHEMAS',
+	]
+)
+
+#start_date = datetime.date( year = 2018, month = 9, day = 1, hour = 8 )
+#end_date = datetime.date( year = 2020, month = 9, day = 1, hour = 8)
+
+#s3 = boto3.resource('s3')
+#for bucket in s3.buckets.all():
+#	print(bucket.name)
+
